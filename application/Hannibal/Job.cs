b673fb502a1aa8f03393647854ff9bdf22dc5e0a
Timo Weggen <timo.weggen@gmail.com>
2025-01-05 15:58:44 +0100

    - structure cleanup, added Api project.

---
+++ application/Hannibal/Job.cs
@@ -0,0 +1,23 @@
+namespace Hannibal;
+
+
+public class Job
+{
+    public int Id { get; set; }
+    
+    public enum JobState
+    {
+        Preparing,
+        Ready,
+        Executing,
+        DoneFailure,
+        DoneSuccess
+    };
+    
+    public JobState State { get; set; }
+    
+    public string FromUri { get; set; }
+    public string ToUri { get; set; }
+    
+    public string ResultCode { get; set; }
+}
