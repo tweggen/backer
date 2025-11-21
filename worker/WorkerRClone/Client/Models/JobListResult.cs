8f60dee739b0b62dcdc07d3e97b35c2f8a2c9659
timow <timo.weggen@gmail.com>
2025-11-21 19:29:43 +0100

    - need job list next.

---
+++ worker/WorkerRClone/Client/Models/JobListResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WorkerRClone.Client.Models;
+
+public class JobListResult
+{
+    [JsonPropertyName("executeId")]
+    public string executeId { get; set; }
+    
+    [JsonPropertyName("finished_ids")]
+    public List<int> finished_ids { get; set; }
+    
+    [JsonPropertyName("jobsids")]
+    public List<int> jobsids { get; set; }
+    
+    [JsonPropertyName("running_ids")]
+    public List<int> running_ids { get; set; }
+}
