011d215eeb7135d92a2cf3f0695a81934e58c0eb
tweggen <timo.weggen@gmail.com>
2026-01-10 13:08:58 +0100

    - added clientid

---
+++ application/Hannibal/Migrations/20260110120829_AddClientId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hannibal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientId",
+                table: "Storages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Storages");
+        }
+    }
+}
