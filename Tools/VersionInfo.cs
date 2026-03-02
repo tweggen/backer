0f1706fe9deb28c17acac629e601b5e436679323
tweggen <timo.weggen@gmail.com>
2026-03-02 15:57:54 +0100

    - added versioninfo.

--- Tools/VersionInfo.cs
+++ Tools/VersionInfo.cs
@@ -1,88 +1,38 @@
+using System.Reflection;
+
 namespace Tools;
 
 /// <summary>
-/// Provides version information about the application
+/// Provides version information about the application, embedded at build time.
 /// </summary>
 public class VersionInfo
 {
     public string? GitCommitShort { get; set; }
     public string? GitCommitFull { get; set; }
     public string? GitBranch { get; set; }
-    public DateTime BuildTime { get; set; } = DateTime.UtcNow;
+    public DateTime BuildTime { get; set; }
 
     /// <summary>
-    /// Attempts to get git version information from the working directory
+    /// Gets version information that was embedded into the assembly at build time
+    /// via AssemblyMetadata attributes (see Directory.Build.props).
     /// </summary>
     public static VersionInfo GetCurrent()
     {
-        var info = new VersionInfo();
+        var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+        var metadata = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+            .ToDictionary(a => a.Key, a => a.Value);
 
-        try
+        var info = new VersionInfo
         {
-            // Get short commit hash
-            var shortResult = System.Diagnostics.ProcessStartInfo.GetCurrentProcess()?.StartInfo;
-            var shortProcess = new System.Diagnostics.ProcessStartInfo
-            {
-                FileName = "git",
-                Arguments = "rev-parse --short HEAD",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true,
-                WorkingDirectory = Directory.GetCurrentDirectory()
-            };
+            GitCommitShort = metadata.GetValueOrDefault("GitCommitShort"),
+            GitCommitFull = metadata.GetValueOrDefault("GitCommitFull"),
+            GitBranch = metadata.GetValueOrDefault("GitBranch"),
+        };
 
-            using (var process = System.Diagnostics.Process.Start(shortProcess))
-            {
-                if (process != null)
-                {
-                    info.GitCommitShort = process.StandardOutput.ReadToEnd().Trim();
-                    process.WaitForExit(5000);
-                }
-            }
-
-            // Get full commit hash
-            var fullProcess = new System.Diagnostics.ProcessStartInfo
-            {
-                FileName = "git",
-                Arguments = "rev-parse HEAD",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true,
-                WorkingDirectory = Directory.GetCurrentDirectory()
-            };
-
-            using (var process = System.Diagnostics.Process.Start(fullProcess))
-            {
-                if (process != null)
-                {
-                    info.GitCommitFull = process.StandardOutput.ReadToEnd().Trim();
-                    process.WaitForExit(5000);
-                }
-            }
-
-            // Get branch name
-            var branchProcess = new System.Diagnostics.ProcessStartInfo
-            {
-                FileName = "git",
-                Arguments = "rev-parse --abbrev-ref HEAD",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true,
-                WorkingDirectory = Directory.GetCurrentDirectory()
-            };
-
-            using (var process = System.Diagnostics.Process.Start(branchProcess))
-            {
-                if (process != null)
-                {
-                    info.GitBranch = process.StandardOutput.ReadToEnd().Trim();
-                    process.WaitForExit(5000);
-                }
-            }
-        }
-        catch (Exception)
+        if (metadata.TryGetValue("BuildTimestamp", out var ts) &&
+            DateTime.TryParse(ts, null, System.Globalization.DateTimeStyles.RoundtripKind, out var buildTime))
         {
-            // Git command failed or git not installed - info will have nulls
+            info.BuildTime = buildTime;
         }
 
         return info;
